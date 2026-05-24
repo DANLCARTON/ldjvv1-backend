1a7f279d07810366eee1d1a0348a6d2aa994a181
DANLCARTON <ericthiberge@orange.fr>
2026-05-24 13:58:08 +0200

    get et add game

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import * as admin from "firebase-admin"
+
 const {setGlobalOptions} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/https");
 const logger = require("firebase-functions/logger");
@@ -30,3 +32,61 @@ setGlobalOptions({ maxInstances: 10 });
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp()
+
+const realtimeDB = admin.realtimeDB()
+
+// FUNCTIONS
+
+export const getGameList = onRequest({cors: true}, async (request, response) => {
+    try {
+        const snapshot = await realtimeDB.ref("ldjvv1/gamelist").once("value");
+        if (snapshot.exists()) {
+            const gamelist = snapshot.val()
+            const gamelistFormatted = Object.values(gamelist);
+            response.json(gamelistFormatted)
+        } else {
+            response.status(404).send("Aucun jeu récupéré")
+        }
+    } catch (error) {
+        response.status(500).send("Erreur lors de la récupération des jeux -> " + error)
+    }
+})
+
+export const addGAme = onRequest({cors: true}, async (request, response) => {
+    try {
+        const name = request.query.name
+        const platformId = request.query.platformId
+        const statusId = request.query.statusId
+        const releaseDate = request.query.releaseDate
+        const purchaseDate = request.query.purchaseDate
+        const startDate = request.query.startDate
+        const endDate = request.query.endDate
+        const index = request.query.index
+
+        const ref = realtimeDB.ref(`ldjvv1/gamelist/${index}`);
+        const snapshot = await ref.once("value");
+
+        if (!snapshot.exists()) {
+            const newGame = {
+                name,
+                platformId,
+                statusId,
+                releaseDate,
+                purchaseDate,
+                startDate,
+                endDate,
+                index,
+            }
+
+            await ref.set(newGame)
+
+            response.status(201).send("Jeu ajouté") 
+        } else {
+            response.stauts(400).send("Un jeu a déjà cet index")
+        }
+    } catch (error) {
+        response.status(500).send("Erreur lors de l'ajout du jeu -> " + error)
+    }
+})
